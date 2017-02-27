d94903ac3e79264cd8f99c4f215659700c78f6a4
Daniel Hug <daniel@hugwebdesign.com>
2017-02-26 17:20:56 -0800

    add JS file

---
+++ wrapper.js
@@ -0,0 +1,55 @@
+/*
+	input:
+	{
+		cells: array (required),
+		parent: element in document (required),
+		cellWidth: number (in pixels; required),
+		cellHeight: number (in pixels; required),
+		numCols: number (optional),
+		fixedWidth: boolean (optional)
+	}
+*/
+
+var Wrapper = (function() {
+	// Add event listener shortcut
+	function on(target, type, callback, useCapture) {
+		target.addEventListener(type, callback, !!useCapture);
+	}
+
+	return function Wrapper(options) {
+		this.cells = options.cells;
+		this.parent = options.parent;
+		this.cellWidth = options.cellWidth;
+		this.cellHeight = options.cellHeight;
+		this.fixedWidth = this.fixedWidth !== undefined ? this.fixedWidth :
+			options.numCols !== undefined;
+		this.numCols = options.numCols || this.getColCount();
+
+		if (this.fixedWidth) return;
+		var instance = this;
+		on(window, 'resize', function() {
+			var newColCount = instance.getColCount();
+			if (newColCount !== instance.numCols) {
+				instance.numCols = newColCount;
+				instance.init();
+			}
+		});
+	};
+})();
+
+Wrapper.prototype.getColCount = function() {
+	return Math.floor(this.parent.offsetWidth / this.cellWidth);
+};
+
+// calculate proper height of content based on number of columns
+Wrapper.prototype.getContentHeight = function() {
+	return Math.ceil(this.cells.length / this.numCols) * this.cellHeight;
+};
+
+Wrapper.prototype.init = function() {
+	for (var i = 0; i < this.cells.length; i++) {
+		var style = this.cells[i].style;
+		style.left = i % this.numCols * this.cellWidth + 'px';
+		style.top = Math.floor(i / this.numCols) * this.cellHeight + 'px';
+	}
+};
